9d7b195c5c2dd5b900fab8ceed1433eae00048fe
Matthew-Burfield <m1.burfield@gmail.com>
2018-03-03 13:35:42 +1000

    server now retrieves coin data for given symbol on the /add_coin route

--- server.js
+++ server.js
@@ -10,32 +10,80 @@ app.use(bodyParser.json())
 const MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.NODE_ENV === 'production' ? 443 : 3000
 
-const listOfCoins = {
-	BTC: {
-		name: 'Bitcoin',
-		code: 'BTC',
-	},
-	LTC: {
-		name: 'Litecoin',
-		code: 'LTC',
-	},
-	ETC: {
-		name: 'Etherium',
-		code: 'ETC',
-	},
-}
-
-app.get('/fullCoinList', (req, res) => res.json(coins))
+const listOfCoins = {}
 
 app.get('/', (req, res) => res.send('Hello now!'))
 
+app.get('/fullCoinList', (req, res) => res.json(coins))
+
 app.get('/list', (req, res) => res.send(listOfCoins))
 
-app.post('/add_coin', (req, res) => {
-	const coinCode = req.body.coinCode
+app.post('/add_coin', async (req, res) => {
+	const symbol = req.body.symbol
+	listOfCoins[symbol] = symbol
+	if (!symbol) {
+		res.json({
+			Error: 'must supply coin symbol',
+		})
+	}
 	// TODO: Check the database to see if this coin exists
+	try {
+		const database = await MongoClient.connect(MONGO_URI)
+		const historicQuotesCollection = database
+			.db('chart-the-crypto-market')
+			.collection('History')
+		const results = await historicQuotesCollection
+			.find({
+				symbol,
+			})
+			.toArray()
+		if (results.length > 0) {
+			// Yay, we have the coin
+			res.json(results)
+		} else {
+			// We don't have the coin so we definitely need to fetch it
+			axios
+				.get(
+					`https://min-api.cryptocompare.com/data/histoday?fsym=${symbol}&tsym=USD&limit=1`,
+				)
+				.then(result => result.data)
+				.then(async data => {
+					// We have the data, so save back to local db
+					console.log(data)
+					try {
+						const update = await historicQuotesCollection.updateOne(
+							{ symbol },
+							{
+								$set: {
+									symbol,
+									timeFrom: data.TimeFrom,
+									timeTo: data.TimeTo,
+									history: data.Data,
+									conversionTo: 'USD',
+								},
+							},
+							{
+								upsert: true,
+							},
+						)
+						console.log(update)
+						res.json({
+							success: true,
+						})
+					} catch (err) {
+						console.log(err)
+						res.send(err)
+					}
+				})
+				.catch(err => {})
+		}
+	} catch (err) {
+		console.log(err)
+		res.send(err)
+	}
 
 	// TODO: If coin doesn't exist or if last fetch date is before yesterday, fetch coin again from API
+	// https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10000
 
 	// TODO: Add coin to listOfCoins, return new coin to users
 })
